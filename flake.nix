f6bb6e64b66b305f359e48c219920af0357bdac7
jokub <adomkus.jokubas@gmail.com>
2026-08-19 23:12:07 +0300

    fixed home manager nixpkgs version

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     zen-browser = {
