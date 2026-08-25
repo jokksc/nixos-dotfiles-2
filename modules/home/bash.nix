e09c900342567a03f511029c2357a242af1bb321
jokub <adomkus.jokubas@gmail.com>
2026-08-25 19:36:11 +0300

    some changes

--- modules/home/bash.nix
+++ modules/home/bash.nix
@@ -8,5 +8,8 @@
       nrs = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles-2#jok-nixos";
       syu = "sudo nix flake update --flake ~/nixos-dotfiles-2 && sudo nixos-rebuild switch --flake ~/nixos-dotfiles-2#jok-nixos";
     };
+    initExtra = ''
+      export PATH="$HOME/.local/share/pi-node/current/bin:$PATH"
+    '';
   };
 }
