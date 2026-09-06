747bca400fe8f4fca1fde80003aafa81421ec9e0
jokub <adomkus.jokubas@gmail.com>
2026-09-06 14:44:44 +0300

    fixes

---
+++ modules/nixos/docker.nix
@@ -0,0 +1,5 @@
+{ lib, config, pkgs, ...}:
+  
+{
+  virtualisation.docker.enable = true;
+}
--- modules/nixos/virtualisation.nix
+++ modules/nixos/virtualisation.nix
@@ -2,7 +2,6 @@
   
 {
   virtualisation.podman.enable = true;
-  virtualisation.docker.enable = true;
   
   environment.systemPackages = with pkgs; [
     distrobox
