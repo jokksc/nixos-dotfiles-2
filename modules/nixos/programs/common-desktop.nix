05ccdf0efb309139194ed6c603b513470073405b
jokub <adomkus.jokubas@gmail.com>
2026-09-15 09:23:47 +0300

    structure changes 2

--- modules/nixos/programs/common-desktop.nix
+++ modules/nixos/programs/common-desktop.nix
@@ -1,5 +1,7 @@
-{ lib, config, pkgs, inputs, ...}:
-  
+{ lib, config, pkgs, inputs, myOptions, ...}:
+let
+  primaryUser = myOptions.users.primaryUser;
+in
 {
   programs.firefox.enable = true;
   programs.localsend = {
@@ -7,6 +9,38 @@
     openFirewall = true;
   };
   
+  # ddcutil - brightness controls for monitors
+  boot.kernelModules = [ "i2c-dev" ];
+  hardware.i2c.enable = true; 
+  users.users.${primaryUser} = {
+    extraGroups = [ "i2c" "networkmanager" "video" "audio" "input" "libvirtd" "kvm" ];
+  };
+  
+  services.xserver = {
+    enable = true;
+  };
+
+  nix.settings.extra-substituters = [
+    "https://niri.cachix.org"
+    "https://noctalia.cachix.org"
+  ];
+
+  nix.settings.extra-trusted-public-keys = [
+    "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+    "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+  ];
+  
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+  
+  networking.networkmanager.enable = true;
+  
+  nixpkgs.config.allowUnfree = true;
+  
+  services.printing.enable = true;
+
   environment.systemPackages = with pkgs; [
     vscode
     # vscodium
@@ -19,6 +53,7 @@
     resources
     blackbox-terminal
     python314
+    ddcutil
     # inputs.helix.packages."${pkgs.stdenv.hostPlatform.system}".helix
   ];
 }
--- modules/nixos/programs/common.nix
+++ modules/nixos/programs/common.nix
@@ -1,6 +1,23 @@
-{ lib, config, pkgs, ...}:
+{ lib, config, pkgs, inputs, myOptions, ...}:
   
 {
+
+  nix.settings.extra-substituters = [
+    "https://niri.cachix.org"
+    "https://noctalia.cachix.org"
+  ];
+
+  nix.settings.extra-trusted-public-keys = [
+    "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+    "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+  ];
+
+  networking.networkmanager.enable = true;
+  
+  nixpkgs.config.allowUnfree = true;
+  
+  services.printing.enable = true;
+
   environment.systemPackages = with pkgs; [
     vim 
     wget 
